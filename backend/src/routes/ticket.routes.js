6a4de3814aca24bca295645d80c249461a9e6cde
Pritam Nandi <pritamn016@gmail.com>
2026-06-19 02:05:43 +0530

    test route

--- backend/src/routes/ticket.routes.js
+++ backend/src/routes/ticket.routes.js
@@ -11,6 +11,16 @@ const {
   cancelTicket
 } = require('../controllers/ticket.controller');
 
+//TEMPORARY 
+router.get('/test-cancel', (req, res) => {
+  console.log("TEST ROUTE HIT");
+  res.json({
+    success: true,
+    message: "cancel route deployed"
+  });
+});
+
+
 // POST /api/tickets - Generate a new ticket (authenticated)
 router.post('/', authenticate, generateTicket);
 
